251060b48f97fb520477858a17f0f1294175d83c
shreychugh <shreychugh@gmail.com>
2025-02-16 17:43:02 -0500

    Update Game.jsx

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,13 +4,10 @@ import ScoreBoard from './ScoreBoard';
 
 function Game() {
   const [score, setScore] = useState(0);
-
   return (
     <div className="game">
       <ScoreBoard score={score} />
-      <div className="balloon-container">
-        <Balloon onPop={() => setScore(score + 1)} />
-      </div>
+      <Balloon onPop={() => setScore(score + 1)} />
     </div>
   );
 }
