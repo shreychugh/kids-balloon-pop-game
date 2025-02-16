b425cad0526f9075231c1d73c763b71c96567dec
shreychugh <shreychugh@gmail.com>
2025-02-16 17:18:50 -0500

    Update Balloon.jsx

--- src/components/Balloon.jsx
+++ src/components/Balloon.jsx
@@ -1,1 +1,23 @@
+import React, { useState } from 'react';
 
+function Balloon({ onPop }) {
+  const [isPopped, setIsPopped] = useState(false);
+
+  const handleClick = () => {
+    if (!isPopped) {
+      setIsPopped(true);
+      onPop();
+    }
+  };
+
+  return (
+    <div 
+      className={`balloon ${isPopped ? 'popped' : ''}`}
+      onClick={handleClick}
+    >
+      🎈
+    </div>
+  );
+}
+
+export default Balloon;
