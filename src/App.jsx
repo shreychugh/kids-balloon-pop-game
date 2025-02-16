375db7e03d0994e85b086dfa7a008ee5f7d612ed
shreychugh <shreychugh@gmail.com>
2025-02-16 17:17:28 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import Game from './components/Game';
 
+function App() {
+  return (
+    <div className="app">
+      <h1>Balloon Pop Game</h1>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
